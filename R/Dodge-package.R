71050b496d0ef4544bc3f0952910a1d6fff4e617
Jonathan Godfrey <a.j.godfrey@massey.ac.nz>
2018-05-29 13:58:30 +1200

    updated package documentation files

--- R/Dodge-package.R
+++ R/Dodge-package.R
@@ -1,20 +1,17 @@
-
-
-
 #' Acceptance sampling functions
 #' 
 #' A number of sampling plans can be compared for their operating
 #' characteristics and other commonly used functions.
 #' 
 #' \tabular{ll}{ Package: \tab Dodge\cr Type: \tab Package\cr Version: \tab
-#' 0.8\cr Date: \tab 2013-09-18\cr License: \tab GPL\cr LazyLoad: \tab yes\cr }
+#' 0.9\cr Date: \tab 2018-05-28\cr License: \tab GPL\cr LazyLoad: \tab yes\cr }
 #' 
 #' @name Dodge-package
 #' @aliases Dodge-package Dodge
 #' @docType package
 #' @author Raj Govindaraju and Jonathan Godfrey
 #' 
-#' Maintainer: Jonathan Godfrey <a.j.godfrey@@massey.ac.nz>
+#' Maintainer: A. Jonathan R. Godfrey <a.j.godfrey@@massey.ac.nz>
 #' @references Dodge
 #' @keywords package
 #' @importFrom graphics axis legend par plot text title
